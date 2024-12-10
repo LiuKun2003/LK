5f999d1ef11aa032af8ccb90968a53d189eb082a
yan <2265478213@qq.com>
2024-12-10 20:25:18 +0800

    Change SingletonCollector.cs

--- CommonLibrary/Other/SingletonCollector.cs
+++ CommonLibrary/Other/SingletonCollector.cs
@@ -6,9 +6,9 @@ namespace LK
     /// <summary>
     /// 单例收集器，此类实现单例模式并且保存其他类型作为唯一实例
     /// </summary>
-    internal sealed class SingletonCollector
+    public sealed class SingletonCollector
     {
-        private readonly static Lazy<SingletonCollector> m_instance = new Lazy<SingletonCollector>(() =>
+        private readonly static Lazy<SingletonCollector> s_instance = new Lazy<SingletonCollector>(() =>
         {
             SingletonCollector collector = new SingletonCollector();
             collector.Initialize();
@@ -20,40 +20,24 @@ namespace LK
         /// <summary>
         /// 获取<see cref="SingletonCollector"></see>唯一实例。
         /// </summary>
-        internal static SingletonCollector Instance => m_instance.Value;
+        public static SingletonCollector Instance => s_instance.Value;
 
-        private ConcurrentDictionary<Type, IController> m_Controllers;
-        private ConcurrentDictionary<Type, IModel> m_Models;
+        private ConcurrentDictionary<Type, object> _objects;
 
         /// <summary>
         /// 初始化单例收集器。
         /// </summary>
         private void Initialize()
         {
-            m_Controllers = new ConcurrentDictionary<Type, IController>();
-            m_Models = new ConcurrentDictionary<Type, IModel>();
+            _objects = new ConcurrentDictionary<Type, object>();
         }
 
         /// <summary>
-        /// 获取保存的控制器类型实例。
+        /// 获取保存的类型的实例。
         /// </summary>
-        /// <returns>控制器的实例</returns>
-        internal T GetController<T>() where T : class, IController, new()
+        public T GetController<T>() where T : class, new()
         {
-            return m_Controllers.GetOrAdd(typeof(T), (type) =>
-            {
-                T res = new T();
-                return res;
-            }) as T;
-        }
-
-        /// <summary>
-        /// 获取保存的数据模型类型实例。
-        /// </summary>
-        /// <returns>数据模型的实例</returns>
-        internal T GetModel<T>() where T : class, IModel, new()
-        {
-            return m_Models.GetOrAdd(typeof(T), (type) =>
+            return _objects.GetOrAdd(typeof(T), (type) =>
             {
                 T res = new T();
                 return res;
